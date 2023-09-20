0a1424ad6dffca0bb8c649894ea0980f2299f87b
Anna Pominchuk <pominchuk.anna@gmail.com>
2023-09-20 23:12:54 +0200

    Load csv in transaction, abort transaction if there is an error in csv, made all shop schema fiels required

---
+++ src/db.ts
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI : '';
+
+mongoose.connect(URI);
+
+module.exports = mongoose;
--- src/model/shop.ts
+++ src/model/shop.ts
@@ -1,14 +1,32 @@
 const mongoose = require('mongoose')
 
 const ShopSchema = new mongoose.Schema({
-    name: String,
-    adress: String,
-    amount: Number,
+    name: {
+      type: String,
+      required: true
+    },
+    adress: {
+      type: String,
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
     m_id: Number,
     t_id: Number,
-    county_id: Number,
-    latitude: Number, 
-    longitude: Number
+    county_id: {
+      type: Number,
+      required: true
+    },
+    latitude: {
+      type: Number,
+      required: true
+    },
+    longitude: {
+      type: Number,
+      required: true
+    }
 })
 
 module.exports = mongoose.model('Shop', ShopSchema)
